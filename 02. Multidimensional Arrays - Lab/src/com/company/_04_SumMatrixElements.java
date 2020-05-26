1af6bd8834211d5a928ae6c1500d57393378dd8f
koceto <thekoceto@mail.bg>
2020-05-27 00:56:15 +0300

    update

--- 02. Multidimensional Arrays - Lab/src/com/company/_04_SumMatrixElements.java
+++ 02. Multidimensional Arrays - Lab/src/com/company/_04_SumMatrixElements.java
@@ -11,12 +11,12 @@ public class _04_SumMatrixElements {
                 .mapToInt(Integer::parseInt).toArray();
         int sum = 0;
 
-        int [][] matrix = new int[n[0]][n[1]];
+        //int [][] matrix = new int[n[0]][n[1]];
         for (int i = 0; i < n[0]; i++) {
             int[] current = Arrays.stream(console.nextLine().split(",\\s+"))
                     .limit(n[1]).mapToInt(Integer::parseInt).toArray();
-            matrix[i] = current;
-            sum += Arrays.stream(matrix[i]).sum();
+            //matrix[i] = current;
+            sum += Arrays.stream(current).sum();
         }
         System.out.println(n[0]);
         System.out.println(n[1]);
