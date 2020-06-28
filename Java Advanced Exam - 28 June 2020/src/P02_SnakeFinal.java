665ed59bd205c30c693f4dc04225a3808c279b23
koceto <thekoceto@mail.bg>
2020-06-28 18:36:05 +0300

    update

--- Java Advanced Exam - 28 June 2020/src/P02_SnakeFinal.java
+++ Java Advanced Exam - 28 June 2020/src/P02_SnakeFinal.java
@@ -3,9 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class P02_SnakeFinal {
-    private static final char head = 'S';
-    private static final char tail = '.';
+    private static final char snakeHead = 'S';
+    private static final char snakeTail = '.';
     private static final char burrow = 'B';
+    private static final char food = '*';
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -14,18 +15,18 @@ public class P02_SnakeFinal {
         int[] bomb = new int[2];
 
         char[][] matrix = new char[size][size];
-        int eaten = 0;
+        int eatenFood = 0;
         for (int i = 0; i < size; i++) {
             String row = reader.readLine();
             matrix[i] = row.toCharArray();
-            if (row.indexOf(head) != -1)
-                bomb = new int[]{i, row.indexOf(head)};
+            if (row.indexOf(snakeHead) != -1)
+                bomb = new int[]{i, row.indexOf(snakeHead)};
         }
-        matrix[bomb[0]][bomb[1]] = tail;
+        matrix[bomb[0]][bomb[1]] = snakeTail;
 
         boolean validIndex = true;
 
-        while (eaten < 10) {
+        while (eatenFood < 10) {
 
             switch (reader.readLine()) {
                 case "up":
@@ -48,27 +49,27 @@ public class P02_SnakeFinal {
             if (!validIndex)
                 break;
             if (matrix[bomb[0]][bomb[1]] == burrow) {
-                matrix[bomb[0]][bomb[1]] = tail;
+                matrix[bomb[0]][bomb[1]] = snakeTail;
                 for (int i = 0; i < matrix.length; i++)
                     for (int j = 0; j < matrix[i].length; j++)
                         if (matrix[i][j] == burrow)
                             bomb = new int[]{i, j};
 
-            }else if (matrix[bomb[0]][bomb[1]] == '*'){
-                eaten++;
+            }else if (matrix[bomb[0]][bomb[1]] == food){
+                eatenFood++;
             }
-            matrix[bomb[0]][bomb[1]] = tail;
+            matrix[bomb[0]][bomb[1]] = snakeTail;
         }
         StringBuilder print = new StringBuilder();
         if (validIndex)
-            matrix[bomb[0]][bomb[1]] = head;
+            matrix[bomb[0]][bomb[1]] = snakeHead;
         else
             print.append("Game over!").append(System.lineSeparator());
 
-        if (eaten == 10)
+        if (eatenFood == 10)
             print.append("You won! You fed the snake.").append(System.lineSeparator());
 
-        print.append("Food eaten: ").append(eaten).append(System.lineSeparator());
+        print.append("Food eaten: ").append(eatenFood).append(System.lineSeparator());
 
         for (char[] row : matrix) {
             for (char ch : row)
